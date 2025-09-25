6be2124214a800601a58b689006ae972ee2bea31
kasiromo <kasijromo@gmail.com>
2025-09-25 17:15:40 -0400

    Add files via upload

--- em/6020E/mod-two-exercises/conditionals/js/conditionals.js
+++ em/6020E/mod-two-exercises/conditionals/js/conditionals.js
@@ -1,1 +1,27 @@
+let dropdown = document.querySelector("#userInput");
 
+for (let i = 0; i <= 100; i++) {
+    dropdown.innerHTML += `<option value="${i}">${i}</option>`;
+}
+
+function compare() {
+    let answer = Number(document.querySelector("#userInput").value);
+    let response = document.querySelector("#response");
+
+    switch (true) {
+        case (answer === 24):
+            response.innerHTML = "Correct!";
+            break;
+        case (answer < 10):
+            response.innerHTML = answer + " is way too low.";
+            break;
+        case (answer < 24):
+            response.innerHTML = answer + " is too low but close.";
+            break;
+        case (answer > 40):
+            response.innerHTML = answer + " is way too high.";
+            break;
+        default:
+            response.innerHTML = answer + " is too high but close.";
+    }
+}
