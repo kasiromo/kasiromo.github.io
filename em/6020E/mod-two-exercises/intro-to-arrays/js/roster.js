3b6ff570e50dfd4d1db8bc78e2eabb78dd59db7a
kasiromo <kasijromo@gmail.com>
2025-09-24 20:14:13 -0400

    Add files via upload

--- em/6020E/mod-two-exercises/intro-to-arrays/js/roster.js
+++ em/6020E/mod-two-exercises/intro-to-arrays/js/roster.js
@@ -1,1 +1,8 @@
+let rosterPix = ["brock.jpg", "channing.jpg", "darnell.jpg", "george.jpg", "jalen.jpg", "james.jpg", "jordan.jpg", "kelee.jpg", "kendall.jpg", "kenny.jpg", "ladd.jpg", "lewis.jpg", "nakobe.jpg", "nolan.jpg", "sedrick.jpg", "stetson.jpg", "tate.jpg", "travon.jpg", "warren.jpg"]
+let rosterNames = ["Brock Bowers", "Channing Tindall", "Darnell Washington", "George Pickens", "Jalen Carter", "James Cook", "Jordan Davis", "Kelee Ringo", "Kendall Milton", "Kenny McIntosh", "Ladd McConkey", "Lewis Cine", "Nakobe Dean", "Nolan Smith", "Sedrick Van Pran", "Stetson Bennett", "Tate Ratledge", "Travon Walker", "Warren Brinson"]
 
+let output = document.querySelector('#goDawgs')
+
+output.innerHTML += rosterNames[i] + "<img src=roster/" + rosterPix[i] + ">" // Loads candidate names & images
+
+for (var i=0; i<rosterNames.length; i++) {}
