83ab97120584e85bd858f2f2e35316fedc4ca8d6
kasiromo <kasijromo@gmail.com>
2025-09-26 12:41:22 -0400

    Add files via upload

--- em/6020E/mod-two-exercises/controlling-css-w-js/js/controlling-css-w-js.js
+++ em/6020E/mod-two-exercises/controlling-css-w-js/js/controlling-css-w-js.js
@@ -2,4 +2,8 @@ let sq = document.querySelector('#square');
 
 function changeCSS() {
     sq.style.backgroundColor = 'red';
+    sq.style.left = '150px'; 
+    sq.style.top = '150px';
+    sq.style.transform = 'rotate(45deg)';
+    sq.style.border = '5px solid black';
 }
