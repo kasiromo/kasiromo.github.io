9256469b9925e2328901330f2c399d857f659d15
kasiromo <kasijromo@gmail.com>
2025-10-15 12:47:09 -0400

    Add files via upload

--- em/6020E/mod-two-exercises/trivia-p2/js/random-trivia-2.js
+++ em/6020E/mod-two-exercises/trivia-p2/js/random-trivia-2.js
@@ -1,1 +1,23 @@
+var divLoad = document.querySelector("#mainDiv");
+var theAnswer = [];
 
+function showAnswer(index) {
+    let answerDiv = document.querySelector("#answers" + index);
+    answerDiv.innerHTML = theAnswer[index];
+}
+
+function pt() {
+    $.getJSON("https://the-trivia-api.com/v2/questions/?limit=10", function (data) {
+        divLoad.innerHTML = "";
+        theAnswer = [];
+
+        for (let i = 0; i < 10; i++) {
+            divLoad.innerHTML += "<h3>Category: " + data[i].category + "</h3>";
+            divLoad.innerHTML += data[i].question.text + "<br><br>";
+            divLoad.innerHTML +=
+                "<button onclick='showAnswer(" + i + ")'>Show Answer</button><br>";
+            divLoad.innerHTML += "<br><div id='answers" + i + "'></div><br>";
+            theAnswer.push(data[i].correctAnswer);
+        }
+    });
+}
