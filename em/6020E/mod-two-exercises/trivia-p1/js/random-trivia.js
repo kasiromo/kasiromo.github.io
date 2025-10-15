620987a75b8d4f6db8045c5ec2d557dbd21977c4
kasiromo <kasijromo@gmail.com>
2025-10-15 11:45:00 -0400

    Add files via upload

--- em/6020E/mod-two-exercises/trivia-p1/js/random-trivia.js
+++ em/6020E/mod-two-exercises/trivia-p1/js/random-trivia.js
@@ -1,1 +1,17 @@
+var question = document.querySelector("#question");
+var answer = document.querySelector("#answer");
+var category = document.querySelector("#category");
+var theAnswer = "";
 
+function qa() {
+    $.getJSON('https://the-trivia-api.com/v2/questions/', function(data) {
+        question.innerHTML = data[0].question.text;
+        category.innerHTML = "Category: " + data[0].category.replaceAll("_", " ");
+        theAnswer = data[0].correctAnswer;
+        answer.innerHTML = "";
+    });
+}
+
+function showAnswer() {
+    answer.innerHTML = "Answer: " + theAnswer;
+}
